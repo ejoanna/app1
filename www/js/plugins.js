01c2668f6a8f81610f81d46d870c279c706a0301
ejoanna <eva.joanna@gmail.com>
2015-02-07 18:13:20 -0600

    cambio 10

--- www/js/plugins.js
+++ www/js/plugins.js
@@ -1,9 +1,12 @@
 // JavaScript Document
 function onBatteryStatus(info) {
     // Handle the online event
+	alert("2")
    alert("Level: " + info.level + " isPlugged: " + info.isPlugged);
 }
 function verEstatusBateria(){
-		window.addEventListener("batterystatus", onBatteryStatus, false);
+	alert("1")
+		window.addEventListener("batterystatus", this.onBatteryStatus, false);
+	alert("3")	
 
 }
